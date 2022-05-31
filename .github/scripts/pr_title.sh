37baa5c7af4826a10b0ebd191e4dba29d23d299a
marshall-ku <72745119+marshall-ku@users.noreply.github.com>
2022-05-31 14:05:26 +0900

    Update pr_title.sh

--- .github/scripts/pr_title.sh
+++ .github/scripts/pr_title.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
-type='^(feat|design|style|fix|docs|refactor|test|chore|move): '
-capital='[A-Z]'
-subject=$'[ ~`!@#$%&*()_+=\\{}|;\':",.\/<>?[-^a-zA-Z0-9-]+$'
-regex="$type$capital$subject"
-
-if [[ ${#MESSAGE} -gt 80 ]]; then
-    echo 'Title is too long. Make it up to 80 characters.'
-    exit 1
-fi
-
+regex="^(feat|design|style|fix|docs|refactor|test|chore|move): [A-Z][A-Za-z ]+(\(#[0-9]+\))?$"
 
 if [[ ! $MESSAGE =~ $regex ]]; then
-    if [[ ! $MESSAGE =~ $type ]]; then
-        echo 'Unexpected type'
-        exit 11
-    fi
-
-    if [[ ! $MESSAGE =~ $type$capital ]]; then
-        echo 'Should start with capital letter'
-        exit 12
-    fi
-
-    echo 'Unexpected character(s) in subject'
-    exit 13
+    echo 'Check your title'
+    exit 1
 fi
